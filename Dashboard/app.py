576fc78ac2fd55c44dee5cf1e30e0c2b1cf0e01b
Aline Schneider <alinesviana@hotmail.com>
2020-08-01 11:37:54 -0400

    C:/Program Files/Git/runmodel route runs onClick, returns model_dict sorted a-z.

--- Dashboard/app.py
+++ Dashboard/app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, jsonify, request
+from flask import Flask, render_template, jsonify, request, url_for, redirect
 from flask_sqlalchemy import SQLAlchemy
 from config import password
 import plotly
@@ -6,7 +6,7 @@ import plotly.graph_objs as go
 import pandas as pd
 import numpy as np
 import json
-
+import os
 import nltk
 from nltk import word_tokenize
 from nltk.corpus import stopwords
@@ -48,22 +48,8 @@ def lyrics_BoW(lyrics):
         if word in model_dict.keys():
             model_dict[word] += 1
     
+    print(f"Result from lyrics_BoW: {model_dict}")
     return model_dict
-
-def run_model():
-    global model_dict
-    global genre
-
-    # Updating dictionary with the genre (user input)
-    model_dict["song_genre"] = genre
-    
-    # Creating final DataFrame
-    df = pd.DataFrame(data=model_dict, index=[0])
-
-    # Call the ML Model function within this function
-    modeltest(df)
-
-
 
 def create_plot():
     N = 40
@@ -94,12 +80,14 @@ class Players(db.Model):
 
 @app.route("/")
 def index():
-    graphJSON = create_plot()
-    return render_template("index.html", graphJSON=graphJSON)
+    # graphJSON = create_plot()
+    # return render_template("index.html", graphJSON=graphJSON)
+    return render_template("index.html")
+
 
 @app.route("/lyrics/<userInput>", methods=["GET"])
-
 def post_lyrics(userInput=None):
+
     # jsonify returns a flask.Response() object that already has the appropriate content-type header 'application/json' for use with json responses
     response = jsonify(userInput) 
     response.headers.add("Access-Control-Allow-Origin", "*")
@@ -113,21 +101,79 @@ def post_lyrics(userInput=None):
     #Call function to preprocess lyrics and feed it through ML Model
     lyrics_BoW(lyrics_json)
     
-    return response 
+    return response
+
+# @app.route("/genre", methods=["POST"])
+# def input_genre():
+#     global genre
+#     if request.method == 'POST':
+#         genre = request.form['selectGenre']
+#         print(f"Printing genre: {genre}")
+#         return jsonify(genre)
 
 @app.route("/genre/<userInput>", methods=["GET"])
-
 def input_genre(userInput=None):
     global genre
     response = jsonify(userInput)
     response.headers.add("Access-Control-Allow-Origin", "*")
     genre = json.dumps(userInput)
+    print(f"Printing genre: {genre}")
     return response
 
-print(f"Printing outside of function: {genre}")
+
+# @app.route("/genre", methods=["POST", "GET"])
+# def input_genre():
+#     global genre
+#     if request.method == "POST":
+#         selection = request.form["selectGenre"]
+#         response = jsonify(selection)
+#         response.headers.add("Access-Control-Allow-Origin", "*")
+#         genre = json.dumps(selection)
+#         print(f"Printing genre: {genre}")
+#     return response
+
+# @app.route("/runmodel/<userInput>", methods=["GET"])
+# def run_model(userInput=None):
+#     global model_dict
+#     response = jsonify(userInput)
+#     response.headers.add("Access-Control-Allow-Origin", "*")
+#     print("Entering runmodel route")
+#     print(f"Genre from run_model function: {genre}")
+#     # Updating dictionary with the genre (user input)
+#     model_dict["song_genre"] = genre
+        
+#     # Creating final DataFrame
+#     # df = pd.DataFrame(data=model_dict, index=[0])
+        
+#     # Call the ML Model function within this function
+#     # modeltest(df)
+
+#     return response
+
+@app.route("/runmodel", methods=["GET", "POST"])
+def run_model():
+    global model_dict
+    # global genre
+    
+    if request.method == 'POST':
+        print("Entering runmodel route")
+        print(f"Genre from run_model function: {genre}")
+        # Updating dictionary with the genre (user input)
+        # POSSIBLE PROBLEM: "song_genre" is being inserted in the middle of the dictionary, in alphabetical order
+        model_dict["song_genre"] = genre
+        # Creating final DataFrame
+        # df = pd.DataFrame(data=model_dict, index=[0])
+        
+        # Call the ML Model function within this function
+        # modeltest(df)
+
+        # return render_template('index.html')
+        return model_dict
+    
+
+
 
 @app.route("/tennis_players", methods=['GET'])
-
 def tennis_players():
     players = Players.query.all()
     results = [
@@ -138,6 +184,22 @@ def tennis_players():
     ]
     return {"players": results}
 
+db = SQLAlchemy()
+
+# def query_to_list(query, include_field_names=True):
+#     """Turns a SQLAlchemy query into a list of data values."""
+#     column_names = []
+#     for i, obj in enumerate(query.all()):
+#         if i == 0:
+#             column_names = [c.name for c in obj.__table__.columns]
+#             if include_field_names:
+#                 yield column_names
+#         yield obj_to_list(obj, column_names)
+
+
+# def obj_to_list(sa_obj, field_order):
+#     """Takes a SQLAlchemy object - returns a list of all its data"""
+#     return [getattr(sa_obj, field_name, None) for field_name in field_order]
 
 if __name__ == "__main__":
     app.run(debug=True)

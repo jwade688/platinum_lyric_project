c6c1a841a9897253d8a818b6eae73276499626a6
Aline Schneider <alinesviana@hotmail.com>
2020-08-07 14:09:05 -0400

    Redesigned, display different alert depending on ML prediction value.

--- Dashboard/app.py
+++ Dashboard/app.py
@@ -23,16 +23,20 @@ app.config["SQLALCHEMY_DATABASE_URI"] = f"postgresql://postgres:{password}@local
 # Create a Flask-SQLAlchemy instance
 db = SQLAlchemy(app)
 
-# Declaring global variables
+# Declaring global variables so they can be used outside of functions
 model_dict = {}
 genre = ""
-prediction = ""
+lyrics_json = {}
+# Empty prediction defaults to no alert message in html template ("Hit or "Not a hit" will not show)
+prediction = "False"
+# Empty features_by_year_results list will be filled with data from database.
 features_by_year_results = []
+# Set selected_feature so that features_by_year chart has an initial plot to show:
 selected_feature = "mode"
 
 def lyrics_BoW(lyrics):
     global model_dict
-
+    global prediction
     # Creating dictionary to match trained ML model, with words as keys and filled with zeros
     model_dict = dict.fromkeys(word_columns, 0)
 
@@ -51,39 +55,26 @@ def lyrics_BoW(lyrics):
         if word in model_dict.keys():
             model_dict[word] += 1
     
+    # Feed model_dict into ML Model, returning prediction
     print(f"Result from lyrics_BoW: {model_dict}")
-    return model_dict
 
-def create_plot():
-    N = 40
-    x = np.linspace(0, 1, N)
-    y = np.random.randn(N)
-    df = pd.DataFrame({'x': x, 'y': y}) # creating a sample dataframe
+    prediction = "True"
 
-    data = [
-        go.Bar(
-            x=df['x'], # assign x as the dataframe column 'x'
-            y=df['y']
-        )
-    ]   
-    graphJSON = json.dumps(data, cls=plotly.utils.PlotlyJSONEncoder)
-    return graphJSON
+    return prediction
+
 
 def plot_features_by_year(feature):
     print("Entering plot_features_by_year() function")
     global features_by_year_results
     # When user changes feature in dropdown, function (from route feature_select) that retrieves
     # user's feature selection runs and updates selected_feature variable
-    print(f"Selected feature (plot_features_by_year() function): {selected_feature}")
+    
     # Call function from route that retrieves values from database
-    # Returns list of dictionaries, each dictionary is one element (row)
+    # Returns list of dictionaries, each dictionary is one element (one row)
     features_by_year_results = by_year()
-    print(f"plot_features_by_year()--> features_by_year_results from by_year() function: {features_by_year_results}")
-    print(feature)
+    
     year = [element["year"] for element in features_by_year_results]
-    # feature_values = [element[feature] for element in features_by_year_results]
     feature_values = [element[feature] for element in features_by_year_results]
-
 
     data = [
         go.Bar(
@@ -99,37 +90,12 @@ def plot_features_by_year(feature):
 def plot_bubble_chart():
     print("Entering plot_bubble_chart() function.")
     global features_by_year_results
-    print(f"plot_bubble_chart()--> features_by_year_results: {features_by_year_results}")
+    # print(f"plot_bubble_chart()--> features_by_year_results: {features_by_year_results}")
 
-    # features_by_year_results = by_year()
     year = [element["year"] for element in features_by_year_results]
     energy = [element["energy"] for element in features_by_year_results]
     acoustiness = [element["acoustiness"] for element in features_by_year_results]
     loudness = [element["loudness"] for element in features_by_year_results]
-
-    # graph = [
-    #     dict(
-    #         data = go.Scatter[
-    #             dict(
-    #                 x = year,
-    #                 y = loudness,
-    #                 mode = "markers",
-    #                 marker = dict(
-    #                     size = [n*100 for n in energy],
-    #                     color = acoustiness,
-    #                     colorscale='Viridis'
-    #                     )
-    #             )
-    #         ],
-
-    #         layout = dict(
-    #             title = 'Bubble Chart Title',
-    #             xaxis_title = "X Axis Title",
-    #             yaxis_title = "X Axis Title"
-    #         )
-    #     )
-    # ]
-
 
     data = [
         go.Scatter(
@@ -144,29 +110,24 @@ def plot_bubble_chart():
         )
     ]
 
-    # layout = dict(
-    #             title = 'Bubble Chart Title',
-    #             xaxis_title = "X Axis Title",
-    #             yaxis_title = "X Axis Title"
-    #         )
+    layout = dict(
+        title = "Bubble Chart Title",
+        xaxis = dict(
+            title_text = "Year"
+        ),
+        yaxis = dict(
+            title_text = "Loudness"
+        )
+    )
 
-    bubble_chart_JSON = json.dumps(data, cls=plotly.utils.PlotlyJSONEncoder)
 
-    print(f"Return bubble_chart_JSON: {bubble_chart_JSON}")
+    bubble_graph = [data, layout]
+
+    bubble_chart_JSON = json.dumps(bubble_graph, cls=plotly.utils.PlotlyJSONEncoder)
+
+    # print(f"Return bubble_chart_JSON: {bubble_chart_JSON}")
 
     return bubble_chart_JSON
-
-class Players(db.Model):
-    __tablename__ = 'players'
-
-    player_id = db.Column(db.Integer, primary_key=True)
-    first_name = db.Column(db.String())
-    last_name = db.Column(db.String())
-    hand = db.Column(db.String())
-    country_code = db.Column(db.String())
-
-    def __repr__(self): 
-        return f"<Customer {self.first_name}>"
 
 
 class Byyear(db.Model):
@@ -201,110 +162,43 @@ def index():
     features_by_year_JSON = plot_features_by_year(selected_feature)
     bubble_chart_JSON = plot_bubble_chart()
     print("Back to route /")
-    print(f"Return bubble_chart_JSON from route /: {bubble_chart_JSON}")
+    # print(f"Return bubble_chart_JSON from route /: {bubble_chart_JSON}")
     return render_template("index.html", features_by_year_JSON=features_by_year_JSON, bubble_chart_JSON=bubble_chart_JSON)
 
 
-@app.route("/lyrics/<userInput>", methods=["GET"])
-def post_lyrics(userInput=None):
 
-    # jsonify returns a flask.Response() object that already has the appropriate content-type header 'application/json' for use with json responses
-    response = jsonify(userInput) 
-    response.headers.add("Access-Control-Allow-Origin", "*")
+@app.route("/get_lyrics")
+def get_lyrics():
+    global prediction
+    print("Entering /get_lyrics route")
+    # Retrieve user input containing lyrics
+    lyrics_input = request.args.get('get-lyrics', 0, type=str)
+    lyrics = json.dumps(lyrics_input)
+    # Call lyrics_BoW function to process lyrics and run ML model, returning prediction
+    prediction = lyrics_BoW(lyrics)
+    print(f"Prediction from /get_lyrics route: {prediction}")
+    return jsonify(result=prediction)
     
-    # return an encoded string.
-    lyrics_json = json.dumps(userInput) 
-    
-    # Print user's input to the terminal
-    print(f"Printing response: {lyrics_json}") 
-    
-    #Call function to preprocess lyrics and feed it through ML Model
-    lyrics_BoW(lyrics_json)
-    
-    return response
 
-# @app.route("/genre", methods=["POST"])
-# def input_genre():
+# @app.route("/genre/<userInput>", methods=["GET"])
+# def input_genre(userInput=None):
 #     global genre
-#     if request.method == 'POST':
-#         genre = request.form['selectGenre']
-#         print(f"Printing genre: {genre}")
-#         return jsonify(genre)
-
-@app.route("/genre/<userInput>", methods=["GET"])
-def input_genre(userInput=None):
-    global genre
-    response = jsonify(userInput)
-    response.headers.add("Access-Control-Allow-Origin", "*")
-    genre = json.dumps(userInput)
-    print(f"Printing genre: {genre}")
-    return response
-
-
-# @app.route("/genre", methods=["POST", "GET"])
-# def input_genre():
-#     global genre
-#     if request.method == "POST":
-#         selection = request.form["selectGenre"]
-#         response = jsonify(selection)
-#         response.headers.add("Access-Control-Allow-Origin", "*")
-#         genre = json.dumps(selection)
-#         print(f"Printing genre: {genre}")
-#     return response
-
-# @app.route("/runmodel/<userInput>", methods=["GET"])
-# def run_model(userInput=None):
-#     global model_dict
 #     response = jsonify(userInput)
 #     response.headers.add("Access-Control-Allow-Origin", "*")
-#     print("Entering runmodel route")
-#     print(f"Genre from run_model function: {genre}")
-#     # Updating dictionary with the genre (user input)
-#     model_dict["song_genre"] = genre
-        
-#     # Creating final DataFrame
-#     # df = pd.DataFrame(data=model_dict, index=[0])
-        
-#     # Call the ML Model function within this function
-#     # modeltest(df)
-
+#     genre = json.dumps(userInput)
+#     print(f"Printing genre: {genre}")
 #     return response
 
-@app.route("/runmodel", methods=["GET", "POST"])
-def run_model():
-    global model_dict
-    global prediction
-    # global genre
-    
-    if request.method == 'POST':
-        print("Entering runmodel route")
-        print(f"Genre from run_model function: {genre}")
-        # Updating dictionary with the genre (user input)
-        # POSSIBLE PROBLEM: "song_genre" is being inserted in the middle of the dictionary, in alphabetical order
-        model_dict["song_genre"] = genre
-        # Creating final DataFrame to pass into the model function
-        # df = pd.DataFrame(data=model_dict, index=[0])
-        
-        # Call the ML Model function within this function
-        # modeltest(df)
-        # ML Model function will return binary prediction
-        prediction = 0
-
-        # return jsonify(prediction)
-    return render_template("index.html", prediction=prediction)
 
 @app.route("/feature_select/<userInput>", methods=["GET"])
 def input_feature(userInput=None):
-    print("Entering input_feature() function in /feature_select route")
-    
+        
     global selected_feature
     response = jsonify(userInput)
     response.headers.add("Access-Control-Allow-Origin", "*")
     selected_feature = json.dumps(userInput).strip('"')
     
-    print(f"Selected feature (input_feature() function): {selected_feature}")
-
-    # Call plot function to create plot using selected_feature variable
+        # Call plot function to create plot using selected_feature variable
     features_by_year_JSON = plot_features_by_year(selected_feature)
 
     return render_template("index.html", features_by_year_JSON=features_by_year_JSON)
@@ -313,7 +207,6 @@ def input_feature(userInput=None):
 @app.route("/features_by_year")
 def by_year():
     global features_by_year_results
-    print("Entering by_year() function in /features_by_year route to pull from Postgres")
     # Query database for all values from features_by_year table (defined in Byyear Class)
     features_by_year = Byyear.query.all()
     features_by_year_results = [
@@ -330,7 +223,7 @@ def by_year():
 
         } for year in features_by_year
     ]
-    print("Returning features_by_year_results from by_year() function")
+
     return features_by_year_results
 
 
